d939f7405dcb52eaaddee703c5ab99d40dbdd763
RNakahara <pikumin55@hotmail.com>
2020-04-05 21:41:12 +0900

    webcam list の機能を追加

--- Main.m
+++ Main.m
@@ -1,16 +1,18 @@
-%% 初期化
+%% initialize
 clear;
 
-%% モデルの選択（１つだけ有効化する）
-%net = DLCameraAlexnet;     %calc_score line 66で落ちる
+%% model selection
+%net = DLCameraAlexnet;     %calc_score line 66 bug
 net = DLCameraGooglenet;
 %net = DLCameraMobilenetv2;
 %net = DLCameraResnet18;
 %net = DLCameraResnet101;
 %net = DLCameraSqueezenet;
 
-%% モデルの初期化
-net.initialize;
+%% model initialization
+% if you have multiple camera, check webcamlist and detect the correct number.
+% wencamlist % 
+net.initialize(1);
 
-%% 実行
+%% main run
 net.run;
